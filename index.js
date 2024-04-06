a446dd771adcfafd59528e803261fc7f499912cf
sasahershko <sarayhershko@gmail.com>
2024-04-06 13:24:09 +0200

    .

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// var checkbox = document.getElementById("darkModeOn");
-
-// checkbox.addEventListener("change", function() {
-//     document.body.classList.toggle("dark-theme");
-// });
+//
 document.addEventListener('DOMContentLoaded', function() {
     var checkbox = document.getElementById("darkModeOn");
   
